62caa0967ae9d09db71bc6e2b63cd573e6690b41
Chocoford <chocoford@icloud.com>
2025-11-13 16:17:22 +0800

    Update NanoBananaMessageExtra.swift

--- Sources/LLMCore/Models/Extended/NanoBananaMessageExtra.swift
+++ Sources/LLMCore/Models/Extended/NanoBananaMessageExtra.swift
@@ -38,3 +38,35 @@ public struct NanoBananaMessageExtra: Decodable, Sendable {
         }
     }
 }
+
+public struct NanoBananaStreamExtra: Decodable, Sendable {
+    public var choices: [Choice]
+    
+    public struct Choice: Decodable, Sendable {
+        public var delta: Message
+        
+        public struct Message: Decodable, Sendable {
+            public var images: [Image]?
+            
+            public struct Image: ContentModel, Sendable {
+                public var type: ImageType
+                public var imageURL: ImageURL
+                public var index: Int
+                
+                public enum ImageType: String, ContentModel, Sendable {
+                    case imageURL = "image_url"
+                }
+                
+                public enum CodingKeys: String, CodingKey {
+                    case type
+                    case imageURL = "image_url"
+                    case index
+                }
+                
+                public struct ImageURL: ContentModel, Sendable {
+                    public var url: String
+                }
+            }
+        }
+    }
+}
